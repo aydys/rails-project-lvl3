7168d20448990c498f26e451689062b06be6b39f
aidys23 <aidongak23@gmail.com>
2022-04-14 12:38:08 +0700

    add paths with web module to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  
+
+  scope module: 'web' do
+    root 'home#index'
+    post 'auth/:provider', to: 'auth#request', as: :auth_request
+    get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth  
+  end
 end
