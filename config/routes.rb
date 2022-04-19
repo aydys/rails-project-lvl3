db3214c5af3155bb524d271e285f7771aa060d9f
aidys23 <aidongak23@gmail.com>
2022-04-19 23:03:41 +0700

    add bulletin resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   scope module: 'web' do
-    root 'home#index'
+    root 'bulletins#index'
     post 'auth/:provider', to: 'auth#request', as: :auth_request
     get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
 
     resource :session, only: %i[destroy]
+
+    resources :bulletins, only: %i[index new create]
   end
 end
