5a28bf65c04ddc7b9183eed857f6fb8f233ee049
aidys23 <aidongak23@gmail.com>
2022-04-20 20:56:32 +0700

    add admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  scope module: 'web' do
+  scope module: :web do
     root 'bulletins#index'
     post 'auth/:provider', to: 'auth#request', as: :auth_request
     get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
 
     resource :session, only: %i[destroy]
-
     resources :bulletins, only: %i[index new create show]
+
+    namespace :admin do
+      root 'bulletins#moderate'
+
+      resources :bulletins, only: :index
+      resources :users, only: %i[edit destroy]
+      resources :categories
+    end
   end
 end
