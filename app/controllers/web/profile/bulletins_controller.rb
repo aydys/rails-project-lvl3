a42c2dcb76b9e2b00cef6be9eb9f46f08847afb4
aidys23 <aidongak23@gmail.com>
2022-04-26 22:58:53 +0700

    create search form to profile root

--- app/controllers/web/profile/bulletins_controller.rb
+++ app/controllers/web/profile/bulletins_controller.rb
@@ -1,5 +1,6 @@
 class Web::Profile::BulletinsController < Web::Profile::ApplicationController
   def index
-    @bulletins = current_user.bulletins.order(created_at: :desc)
+    @query = current_user.bulletins.by_recently_created.ransack(params[:q])
+    @bulletins = @query.result
   end
 end
