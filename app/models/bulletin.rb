f6a9674a89291c06f383dbd8d771c5030e970ff0
aidys23 <aidongak23@gmail.com>
2022-04-28 17:40:38 +0700

    fix env via rubocop

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -11,7 +11,7 @@ class Bulletin < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 1000 }
   validates :image, attached: true, content_type: %i[png jpeg jpg],
-                    size: { less_than: 5.megabytes, message: 'is not given between size' }
+                    size: { less_than: 5.megabytes }
 
   scope :by_recently_created, -> { order(created_at: :desc) }
 
