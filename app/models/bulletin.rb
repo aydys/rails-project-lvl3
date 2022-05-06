27319ef96b470b0ff3aa83c46b79f0402c7928ef
aidys23 <aidongak23@gmail.com>
2022-05-06 16:56:26 +0700

    add validations to bulletin & user

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -8,7 +8,7 @@ class Bulletin < ApplicationRecord
 
   has_one_attached :image
 
-  validates :title, presence: true, length: { maximum: 50 }
+  validates :title, presence: true, length: { in: 3..50 }
   validates :description, presence: true, length: { maximum: 1000 }
   validates :image, attached: true, content_type: %i[png jpeg jpg],
                     size: { less_than: 5.megabytes }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors.add attribute, (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ApplicationRecord
   has_many :bulletins, dependent: :destroy
+
+  validates :name, presence: true, length: { in: 1..256 }
+  validates :email, presence: true, uniqueness: true, email: true
 end
