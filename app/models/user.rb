54098aabf5d99262ad2411cc19adda3177441eea
aidys23 <aidongak23@gmail.com>
2022-05-07 22:02:46 +0700

    remove email format validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,8 @@
 # frozen_string_literal: true
 
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    return if value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-
-    record.errors.add attribute, (options[:message] || 'is not an email')
-  end
-end
-
 class User < ApplicationRecord
   has_many :bulletins, dependent: :destroy
 
   validates :name, presence: true, length: { in: 1..256 }
-  validates :email, presence: true, uniqueness: true, email: true
+  validates :email, presence: true, uniqueness: true
 end
