9d06201ff22342e33eeb6ad90d38c0f6131a9ebc
aidys23 <aidongak23@gmail.com>
2022-05-18 15:30:18 +0700

    create fallback for pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@
 class ApplicationController < ActionController::Base
   include AuthConcern
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = t('fail_authorization')
+    redirect_back(fallback_location: root_path)
+  end
 end
